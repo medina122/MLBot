ac8bec5d74b25b94a35306f2795319b9253383ae
Medina <luisangelmedina00@gmail.com>
2021-12-16 04:57:27 +0100

    asda

--- publicar.py
+++ publicar.py
@@ -1,7 +1,7 @@
 import pyautogui as bot, os, pyperclip
 from time import sleep
 from funciones import locate_image, read_txt, telegram_report
-from new_product_list import listar_productos
+from enlistar_productos import listar_productos
 
 def post(path):
 
