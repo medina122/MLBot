2b4b3fa535305e832b73247bdd5f229982dd14e1
2 <1>
2021-12-21 10:35:10 -0500

    aSDASDAASD

--- publicar.py
+++ publicar.py
@@ -283,5 +283,5 @@ if __name__ == '__main__':
         elif int(opcion) == 3:
             path = input('Ingrese ruta de de productos a listar: ')
             listar_productos(path)
-            telegram_report(read_txt(mainfolder, 'productos'), '-1001781252897') # BOT
+            
             
