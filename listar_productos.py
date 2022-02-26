9e416f59d0a54d2d6da6e19800b51ffc5d1e2d48
Medina <luisangelmedna00@gmail.com>
2022-02-26 17:56:43 -0400

    Arreglos

--- listar_productos.py
+++ listar_productos.py
@@ -1,4 +1,18 @@
 import os
+from time import sleep
+
+def leer_archivo_del_producto(ruta, nombre_del_archivo):
+    archivo = os.path.join(ruta, nombre_del_archivo) + '.txt'
+    
+    with open(archivo, 'r', encoding='utf8') as file: 
+        data = file.read()
+        return data
+
+def crear_reporte(contenido):
+    
+    with open('reporte.txt', 'a', encoding='utf8') as file:
+        file.write(contenido)
+        file.close()
 
 def listar_productos():
 
@@ -10,10 +24,11 @@ def listar_productos():
         
         subcarpetas = os.path.join(carpeta_principal, categoria)
         
-        for producto in os.listdir(subcarpetas):
+        for subcarpeta in os.listdir(subcarpetas):
         
-            path = os.path.join(subcarpetas, producto)
-            print(path)
-
+            ruta_de_la_subcarpeta = os.path.join(subcarpetas, subcarpeta)
+            informacion_requerida = f"{leer_archivo_del_producto(ruta_de_la_subcarpeta, 'titulo')} -- {leer_archivo_del_producto(ruta_de_la_subcarpeta, 'precio')}\n"
+            crear_reporte(informacion_requerida)
+            sleep(0.05)
 
 listar_productos()
