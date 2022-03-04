b31e8b01d1ed36c48063b279039d0c1ffa45d943
Medina <luisangelmedna00@gmail.com>
2022-03-04 12:33:16 -0400

    Puliendo el codigo

--- functions.py
+++ functions.py
@@ -7,63 +7,46 @@ import os
 
 colorama.init()
 
+path = os.getcwd()
+tweens = [bot.easeInElastic, bot.easeInBack, bot.easeInBounce, bot.easeInCirc, bot.easeInCubic, bot.easeInExpo, bot.easeInOutBack, bot.easeOutBack, bot.easeOutCirc, bot.easeOutElastic, bot.easeOutCubic, bot.easeOutQuad, bot.easeOutQuint, bot.easeOutQuart, bot.easeOutBounce]
+
+def image_position(img_name: str, conf: float, check: bool):
+    image = os.path.join(path, 'src', img_name) + '.png'
+    print(image)
+    cords = bot.locateOnScreen(image, confidence=conf)
+
+    if cords != (None):
+        print(cords)
+    elif not cords and check:
+        attempt = 0
+        sleep(0.25)
+        while not cords:
+            attempt += 1
+            print(f'Locating {img_name}, attempt: {attempt}')
+            sleep(0.5)
+            cords =  bot.locateOnScreen(image, confidence=conf)
+    else: print(f'Not found: {img_name}')    
+    return cords
+    
+def locate_image(img_name, conf=.8, check=False, move=True, click=True, wait=None, end=None):
+
+    if wait != None: sleep(wait)
+
+    image = image_position(img_name, conf, check)
+
+    try:
+        if image != (None): 
+            if move: bot.moveTo(image, duration=random.randint(10,30)/100, tween=random.choice(tweens))
+            if click: bot.click(image, duration=random.randint(10,30)/100, tween=random.choice(tweens))
+    except: 
+        print('Actions cannot be completed')
+    
+    if end != None: sleep(end)
+    return image
+
 def clear():
     import os
     os.system('cls' if os.name == 'nt' else 'clear')
-
-def locate_image(name, move=True, click=True, check=False, co=0.8, wait=0, duration=0.20, NotFound=True):
-    
-    bot.PAUSE = random.randint(5,20)/100
-
-    if wait != 0: sleep(wait)
-
-    data = os.path.join(os.getcwd(), 'src', name)+'.png'
-    
-    if check:
-
-        while True:
-            sleep(0.25)
-            cords = bot.locateOnScreen(data, confidence=co)
-            
-            if cords:
-
-                sleep(0.5)
-
-                if move:
-                    bot.moveTo(cords, duration=duration)
-
-                if click:
-                    bot.click(cords, duration=duration)
-                    sleep(0.20)
-                
-                print(f'Found {name}')
-            
-                return True, cords
-
-            else: 
-                
-                if NotFound:
-                    print(f'Not found {name}')
-
-    else:
-        
-        cords = bot.locateOnScreen(data, confidence=co)
-
-        if cords:
-
-            if move:
-                bot.moveTo(cords, duration=duration)
-
-            if click:
-                bot.click(cords, duration=duration)
-            
-            print(f'Found {name}')
-
-            return True, cords
-        
-        else: 
-            print(f'Not found {name}')
-            return False
 
 def read_txt(path, name, copy=False, paste=False):
 
